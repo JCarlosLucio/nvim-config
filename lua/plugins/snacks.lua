5da4bdd6e8599f1c4e00d6c0782856a26c92215f
Juan Carlos Lucio <c.lucio3@gmail.com>
2025-03-15 15:38:21 -0600

    Hide ignored files on explorer/file finder - Revert to hiding ignored files as they were interfering with file search on explorer/file finder

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,12 +3,12 @@ return {
   opts = {
     picker = {
       hidden = true, -- for hidden files in explorer
-      ignored = true, -- for .gitignore files in explorer
+      -- ignored = true, -- for .gitignore files in explorer
       exclude = { ".git" }, -- for excluding .git dir in explorer
       sources = {
         files = {
           hidden = true, -- for hidden files in File Finder
-          ignored = true, -- for .gitignore files in File Finder
+          -- ignored = true, -- for .gitignore files in File Finder
         },
       },
     },
