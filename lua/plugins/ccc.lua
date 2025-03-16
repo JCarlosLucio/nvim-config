bd6bea2339a1b6013b5fb80fa691f8dd0a10e81c
Juan Carlos Lucio <c.lucio3@gmail.com>
2025-03-16 12:11:07 -0600

    Install ccc (Create Color Code) plugin - Add ccc plugin config

---
+++ lua/plugins/ccc.lua
@@ -0,0 +1,14 @@
+-- Create Color Code - https://github.com/uga-rosa/ccc.nvim
+return {
+  "uga-rosa/ccc.nvim",
+  opts = {
+    highlighter = {
+      auto_enable = true,
+    },
+    alpha_show = true,
+    recognize = {
+      input = true,
+      output = true,
+    },
+  },
+}
