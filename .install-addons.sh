c78619faad77043e604ac8a7d5db4348a7f0b170
Juan Carlos Lucio <c.lucio3@gmail.com>
2025-04-06 14:10:20 -0600

    Add install neovim / kitty

--- .install-addons.sh
+++ .install-addons.sh
@@ -1,4 +1,21 @@
-#!bin/bash
+#!/usr/bin/bash
 
 # Setup additional programs for nvim
 sudo apt install fzf ripgrep fd-find wl-clipboard imagemagick python3-neovim -y
+
+# Install neovim latest - https://github.com/neovim/neovim/blob/master/INSTALL.md#ubuntu
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt update
+sudo apt install neovim
+
+# Install kitty
+sudo apt install kitty
+# Generate kitty default configuration
+# kitty +runpy 'from kitty.config import *; print(commented_out_default_config())'
+# Get kitty themes
+# kitty +kitten themes
+# Make kitty transparent
+# Change the following options in kitty.conf
+# background_opacity 0.8
+# background_blur 16
+
