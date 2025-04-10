fc56878ea3501ffc0c849121e19723067491aa63
Juan Carlos Lucio <c.lucio3@gmail.com>
2025-04-10 01:40:49 -0600

    Install dap plugin - Setup gbd via vscode cpptools extension

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,22 @@
+-- https://github.com/mfussenegger/nvim-dap
+-- Setup gbd via vscode cpptools - https://github.com/mfussenegger/nvim-dap/wiki/C-C---Rust-(gdb-via--vscode-cpptools)
+-- Dap looks for configurations entries in dap.configurations.<filetype> (https://github.com/mfussenegger/nvim-dap/wiki/C-C---Rust-(gdb-via--vscode-cpptools)#configuration)
+-- or .vscode/launch.json (https://github.com/mfussenegger/nvim-dap/blob/master/doc/dap.txt#L1355)
+-- Example config: https://github.com/igorlfs/dotfiles/blob/main/nvim/.config/nvim/lua/plugins/bare/nvim-dap.lua
+
+return {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    -- Runs preLaunchTask / postDebugTask if present
+    { "stevearc/overseer.nvim", config = true },
+  },
+  opts = function()
+    local dap = require("dap")
+    dap.adapters.cppdbg = {
+      id = "cppdbg",
+      type = "executable",
+      -- Requires installation of cpptools extension - https://github.com/mfussenegger/nvim-dap/wiki/C-C---Rust-(gdb-via--vscode-cpptools)#installation
+      command = "~/.config/nvim/vscode-extensions/cpptools/extension/debugAdapters/bin/OpenDebugAD7",
+    }
+  end,
+}
