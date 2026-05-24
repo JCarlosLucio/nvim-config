c0abf9b8caea73da635494080638d5af944cd4e5
Juan Carlos Lucio <c.lucio3@gmail.com>
2026-05-24 16:03:52 -0600

    Organize imports on save with ruff using conform.nvim - Remove ruff lsp-config that organized imports on save

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,17 @@
+---@module "conform"
+
+return {
+  {
+    "stevearc/conform.nvim",
+    ---@type conform.setupOpts
+    opts = {
+      formatters_by_ft = {
+        python = {
+          "ruff_fix",
+          "ruff_format",
+          "ruff_organize_imports",
+        },
+      },
+    },
+  },
+}
