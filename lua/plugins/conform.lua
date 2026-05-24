8569b5613d3d85666ab382942890bed1d0146e32
Juan Carlos Lucio <c.lucio3@gmail.com>
2026-05-24 16:14:00 -0600

    Add comment about ruff conform config

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,6 +5,7 @@ return {
     "stevearc/conform.nvim",
     ---@type conform.setupOpts
     opts = {
+      -- https://docs.astral.sh/ruff/editors/setup/#neovim
       formatters_by_ft = {
         python = {
           "ruff_fix",
