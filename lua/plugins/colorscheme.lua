2d6ea135ef5a7599713cc691216322562c7ea027
Juan Carlos Lucio <c.lucio3@gmail.com>
2026-05-31 17:41:05 -0600

    Add nightfox colortheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -57,3 +57,22 @@ return {
 --   },
 -- }
 --
+-- return {
+--   "EdenEast/nightfox.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     require("nightfox").setup({
+--       options = {
+--         transparent = true,
+--         dim_inactive = false,
+--         styles = {
+--           comments = "italic",
+--           keywords = "bold",
+--           types = "italic,bold",
+--         },
+--       },
+--     })
+--     vim.cmd("colorscheme duskfox") -- or nightfox nordfox terafox carbonfox
+--   end,
+-- }
