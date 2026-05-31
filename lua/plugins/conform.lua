832861a4c023fb605db61296ede014f1eb4fa940
Juan Carlos Lucio <c.lucio3@gmail.com>
2026-05-31 01:04:16 -0600

    Add formatter for more languages

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -12,7 +12,25 @@ return {
           "ruff_format",
           "ruff_organize_imports",
         },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        svelte = { "prettier" },
+        html = { "prettier" },
+        css = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
       },
+      -- Don't set `opts.format_on_save` for `conform.nvim`.
+      -- LazyVim will use the conform formatter automatically
+      --
+      -- format_on_save = {
+      --   timeout_ms = 500,
+      --   lsp_format = "fallback",
+      --   async = false,
+      -- },
     },
   },
 }
