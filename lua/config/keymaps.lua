421ceeedf4ab17f527a98194b000f91cd5730feb
Juan Carlos Lucio <c.lucio3@gmail.com>
2026-06-10 23:31:52 -0600

    Add keymaps for commenting and opening terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Comment line/selection using "Ctrl+/"
+vim.keymap.set("n", "<C-/>", "gcc", { remap = true, desc = "Comment line" })
+vim.keymap.set("v", "<C-/>", "gc", { remap = true, desc = "Comment selection" })
+
+-- Open terminal using "Ctrl+`" (changed from using "Ctrl+/")
+vim.keymap.set("n", "<C-`>", function()
+  Snacks.terminal.focus(nil, { cwd = LazyVim.root() })
+end, { desc = "Terminal (Root Dir)" })
+vim.keymap.set("t", "<C-`>", function()
+  Snacks.terminal.focus(nil, { cwd = LazyVim.root() })
+end, { desc = "Terminal (Root Dir)" })
