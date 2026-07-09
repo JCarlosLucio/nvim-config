6da3247e7f5557f7bacf832422d7872c630e1b6f
Juan Carlos Lucio <c.lucio3@gmail.com>
2026-07-08 22:21:42 -0600

    Change theme to tokyonight - Add comments for other themes' styles

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,14 @@
--- return {
---   "folke/tokyonight.nvim",
---   opts = {
---     style = "night", -- "moon", "storm", "night", "day"
---     transparent = true,
---     styles = {
---       sidebars = "transparent",
---       floats = "transparent",
---     },
---   },
--- }
+return {
+  "folke/tokyonight.nvim",
+  opts = {
+    style = "moon", -- "moon", "storm", "night", "day"
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
+  },
+}
 --
 -- return {
 --   "maxmx03/fluoromachine.nvim",
@@ -16,7 +16,7 @@
 --   priority = 1000,
 --   config = function()
 --     require("fluoromachine").setup({
---       theme = "delta",
+--       theme = "delta", -- "delta", "retrowave", "fluoromachine"
 --       glow = true,
 --       transparent = true,
 --     })
@@ -25,20 +25,20 @@
 --   end,
 -- }
 --
-return {
-  "eldritch-theme/eldritch.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  config = function()
-    require("eldritch").setup({
-      transparent = true,
-      palette = "default",
-    })
-    vim.cmd("colorscheme eldritch")
-  end,
-}
-
+-- return {
+--   "eldritch-theme/eldritch.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   opts = {},
+--   config = function()
+--     require("eldritch").setup({
+--       transparent = true,
+--       palette = "default", -- "default", "darker", "minimal"
+--     })
+--     vim.cmd("colorscheme eldritch")
+--   end,
+-- }
+--
 -- return {
 --   {
 --     "scottmckendry/cyberdream.nvim",
